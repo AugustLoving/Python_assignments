c8ee8b50ee42ddf1e2f230c4a3bfcb7410c9d3b1
AugustLoving <auglov536@ihmeducation.se>
2024-11-13 14:36:04 +0100

    Add files via upload

---
+++ Uppgifter.py
@@ -0,0 +1,181 @@
+#Upg1
+# usd_rate = 11.25
+# eur_rate = 12.45
+
+# amount_sek = float(input("Ange belopp i SEK: "))
+
+# amount_usd = amount_sek / usd_rate
+# amount_eur = amount_sek / eur_rate
+
+# print(f"{amount_sek} SEK i USD är: {amount_usd}")
+# print(f"{amount_sek} SEK i EUR är: {amount_eur}")
+
+#Upg2
+# ålder = 25
+# is_student = True
+
+# if ålder < 26 or is_student:
+#     print("Berättigad till studentrabbat!")
+# else:
+#     print("Inte berättigad till studentrabbat!")
+
+#Upg3
+# fav_movies = ["Star Wars","Star Trek","Inception","The Shawshank Redemption","The Godfather"]
+
+# for movie in fav_movies:
+#     if "Star" in movie:
+#         print(f"{movie} - Stjärnfilm")
+#     else:
+#         print(movie)
+    
+#Upg4
+# math_students = {"Anna", "Bertil", "Cecilia", "David", "Emma"}
+
+# science_students = {"Cecilia", "David", "Fredrik", "Greta", "Hanna"}
+
+# both_courses = math_students & science_students
+# print(f"Elever som är med i båda kurserna: {both_courses}")
+
+# specific_student = input("Studentnamn: ")
+
+# if specific_student in both_courses:
+#     print(f"{specific_student} är med i båda kurserna")
+# else:
+#     print(f"{specific_student} är inte med i båda kurserna")
+
+#Upg5
+# products = {"mjölk": "25:-", "bröd": "30:-", "kakor": "20:-"}
+
+# search = input("Sök produkt: ")
+
+# if search in products:
+#     print(products.pop(search))
+# else:
+#     print("Produkten finns inte i lager")
+
+#Upg6
+# import random
+# start_number = 10
+
+# while start_number >= 0:
+#     print(start_number)
+#     start_number -= random.randint(1, 3)
+
+#Upg7
+# my_list = ["äpple", "banan", "körsbär", "druva"]
+
+# bokstav = input("Skriv en bokstav: ")
+
+# for ord_i_listan in my_list:
+#     if bokstav in ord_i_listan:
+#         print(ord_i_listan)
+
+#Upg8
+# numbers = [10, -5, 20, -15, 30, -25, 50, -40, 60, -35]
+
+# def is_negative(n):
+#     return n < 0
+
+# negative_numbers = list(filter(is_negative, numbers))
+
+# print(list(negative_numbers))
+
+# print(len(negative_numbers))
+
+#Upg9
+# bok = ("Kingen", "August Löving", 1999)
+
+# print(f"Boken '{bok[0]}' är skriven av {bok[1]} och publicerades år {bok[2]} ")
+
+#Upg 10
+# student_points = {"August": 100, "Willy": 90, "Kaan": 45, "Gurra": 80}
+
+# for name, points in student_points.items():
+#     print(f"{name}: {points} poäng")
+
+# medelpoäng = sum(student_points.values()) / len(student_points)
+# print(f"Medelpoängen är {medelpoäng:.2f}")
+
+
+#Funktion Uppgifter
+#Upg1
+# def hello_world():
+#     print("Hej världen!")
+
+# hello_world()
+
+#Upg2
+# def sum(num1, num2):
+#     return num1 + num2
+
+# resultat = sum(150, 370)
+
+# print(resultat)
+
+#Upg3
+# def addera(num1, num2):
+#     return num1 * num2
+
+# resultat = addera(10, 3)
+
+# print(resultat)
+
+#Upg4
+# def tal():
+#     print("42")
+
+# tal()
+
+#Upg5
+# def person_info(namn, ålder, stad):
+#     return f"Hej, jag heter {namn}, jag är {ålder} år gammal och bor i {stad}." 
+
+# personen = person_info("August", 25, "Västerås")
+
+# print(personen)
+
+#Upg6
+# def name_age(name, age=33):
+#     return f"Mitt namn är {name} jag är {age} år gammal"
+
+# person = name_age("August", )
+
+# print(person)
+
+#Upg7
+# def omkretsen(radie):
+#     omkrets = 2 * 3.14 * radie
+#     return omkrets
+
+# cirkel_radie = 100
+
+# print(omkretsen(cirkel_radie))
+
+#Upg8
+# def sum_numbers(numbers):
+#     return sum(numbers)
+
+# test_list = [10, 20, 30, 40, 50]
+# result = sum_numbers(test_list)
+
+# print(result)
+
+#Upg9
+# def temp(tempen):
+#     if tempen  < 10:
+#         return "Kallt"
+#     elif tempen > 25:
+#         return "Varm"
+#     else:
+#         return "mild"
+    
+# dagens_temp = 11
+
+# print(temp(dagens_temp))
+
+#Upg 10
+def colour():
+   färg = input("Färg: ")
+   print(f"Din favorit färg är: {färg}")
+
+colour()
---
+++ main.py
@@ -0,0 +1,151 @@
+# #for-loop
+# # for element in sekvens:
+#     # Kod att köra för varje element
+names = ["ALice", "Bob", "Charlie"]
+# Iterera genom varje namn i listan
+for name in names:
+    print(name)
+# # resultat:
+# # Alice
+# # Bob
+# # Charlie
+# for i in range(1, 5):  # range(1, 5) ger 1, 2, 3, 4
+#     print(i)
+
+# #while-loop
+# # Starta med en räknare
+# counter = 1
+
+# # Kör loopen så länge räknaren är mindre än eller lika med 5
+# while counter <= 5:
+#     print(counter)
+#     counter += 1  # Öka räknaren med 1
+# # Förväntat resultat:
+# # 1
+# # 2
+# # 3
+# # 4
+# # 5
+
+# #Uppgifter 4
+# #upg1
+# num = [1, 2, 3, 4, 5]
+# total = 0
+
+# for numbers in num:
+#     total += numbers
+   
+# print(total)
+
+# #upg2
+# lista = [3, 12, 7, 21, 5]
+# greatest = lista[0]
+
+# for great in lista:
+#     if great > greatest:
+#         greatest = great
+# print(greatest)
+
+# #upg 3
+# counter = 1
+# while counter <= 10:
+#     print(counter)
+#     counter += 1
+
+# # upg4
+# my_list = [5, 12, 9, 15, 8]
+# answer = 0
+# i = 0
+
+# while i < len(my_list):
+#     if my_list[i] > 10:
+#         answer += my_list[i]
+#     i += 1
+
+# print(f"svaret på upg 4: {answer}") 
+
+# #upg5
+# min_list = [2, 3, 4]
+# result = 1
+
+# for numbers in min_list:
+#     result *= numbers
+    
+# print(f"svaret på upg 5: {result}")
+
+# 3 in [1, 2, 3, 4]  # True
+# 'p' in "python"  # True
+
+# 5 not in [1, 2, 3, 4]  # True
+# 'z' not in "python"  # True
+
+#input
+# namn = input("Vad heter du? ")
+# print(f"Hej, {namn}!")
+
+# print(round(3.14159))  # 3
+# print(round(3.14159, 2)) # 3.14. efter "," hur många decimaler man vill ha
+
+# lista = [1, 2, 3, 4]
+# print(sum(lista))  # 10
+
+# lista = [1, 2, 3, 4]
+# print(max(lista))  # 4
+
+# lista = [1, 2, 3, 4]
+# print(min(lista))  # 1
+
+# lista = [3, 1, 4, 2]
+# print(sorted(lista))  # [1, 2, 3, 4]
+
+# printar True om alla elementen är true
+# lista = [True, True, True]
+# print(all(lista))  # True
+
+# lista = [True, False, True]
+# print(all(lista))
+
+# printar true om minst ett element är true
+# lista = [False, True, False]
+# print(any(lista))  # True
+
+# lista = [False, False, False]
+# print(any(lista))  # False
+
+# zip()
+# Kombinerar två eller fler iterables och returnerar ett objekt av tupel.
+# namn = ["Anna", "Björn", "Carla"]
+# ålder = [25, 30, 35]
+# for person in zip(namn, ålder):
+#     print(person)
+# # ('Anna', 25)
+# # ('Björn', 30)
+# # ('Carla', 35)
+
+# enumerate()
+# Lägger till en indexposition till varje element i en iterable.
+# lista = ["äpple", "banan", "apelsin"]
+# for index, frukt in enumerate(lista):
+#     print(index, frukt)
+# 0 äpple
+# 1 banan
+# 2 apelsin
+
+# isinstance()
+# Kontrollerar om ett objekt är en instans av en viss klass eller typ.
+# x = 5
+# print(isinstance(x, int))  # True
+# print(isinstance(x, str))  # False
+
+# my_tuple = (1, 2, 2, 3)
+# occurrences = my_tuple.count(2)
+# print(occurrences)
+
+### Sammanfattning av slicing
+
+# sekvens[start:stop]`**: Från `start` till men inte inklusive `stop`.
+# sekvens[start:]`**: Från `start` till slutet.
+# sekvens[:stop]`**: Från början till `stop`.
+# sekvens[::step]`**: Hela sekvensen med steg `step`.
+# sekvens[::-1]`**: Hela sekvensen omvänd.
+
