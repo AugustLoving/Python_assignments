2a15c5fe307c841c2c43257c8ce1ac7da228e2d8
AugustLoving <auglov536@ihmeducation.se>
2024-11-14 13:27:56 +0100

    Update classer.py

---
+++ classer.py
@@ -0,0 +1,27 @@
+# class Rektangel:
+#     def __init__(self, längd, bredd):
+#         self.längd = längd
+#         self.bredd = bredd
+#     def area(self,):
+#         arean = self.längd * self.bredd
+#         return arean
+
+
+# min_rektangel = Rektangel(100, 50)
+# min_rektangel2 = Rektangel(200, 10)
+
+# print(min_rektangel.area())
+# print(min_rektangel2.area())
+
+class Bok:
+    def __init__ (self, titel, författare, år):
+        self.titel = titel
+        self.författare = författare
+        self.år = år
+    def beskrivning(self,):
+        print(f"Titel: {self.titel}, Författare: {self.författare} År: {self.år}")
+        
+    
+
+min_bok = Bok("Kingen", "August Löving", 1999)
+min_bok.beskrivning()
