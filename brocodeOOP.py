b739ee5a758d05fecd85a1ba0cd255527052d998
AugustLoving <auglov536@ihmeducation.se>
2024-11-20 10:38:50 +0100

    Update brocodeOOP.py

--- brocodeOOP.py
+++ brocodeOOP.py
@@ -177,46 +177,46 @@
 # circle.describe()
 
 #Polymorphism
-from abc import ABC, abstractmethod
+# from abc import ABC, abstractmethod
 
-class Shape:
+# class Shape:
     
-    @abstractmethod
-    def area(self):
-        pass
+#     @abstractmethod
+#     def area(self):
+#         pass
 
-class Circle(Shape):
-    def __init__(self, radius):
-        self.radius = radius
+# class Circle(Shape):
+#     def __init__(self, radius):
+#         self.radius = radius
 
-    def area(self):
-        return 3.14 * self.radius ** 2
+#     def area(self):
+#         return 3.14 * self.radius ** 2
 
-class Square(Shape):
-    def __init__(self, side):
-        self.side = side
+# class Square(Shape):
+#     def __init__(self, side):
+#         self.side = side
     
-    def area(self):
-        return self.side ** 2
+#     def area(self):
+#         return self.side ** 2
 
-class Triangle(Shape):
-    def __init__(self, base, height):
-        self.base = base
-        self.height = height
+# class Triangle(Shape):
+#     def __init__(self, base, height):
+#         self.base = base
+#         self.height = height
     
-    def area(self):
-        return self.base * self.height * 0.5
+#     def area(self):
+#         return self.base * self.height * 0.5
     
-class Pizza(Circle):
-    def __init__(self, topping, radius):
-        super().__init__(radius)
-        self.topping = topping
+# class Pizza(Circle):
+#     def __init__(self, topping, radius):
+#         super().__init__(radius)
+#         self.topping = topping
         
 
 
-shapes = [Circle(4), Square(5), Triangle(6, 7), Pizza("Skinka", 15)]
+# shapes = [Circle(4), Square(5), Triangle(6, 7), Pizza("Skinka", 15)]
 
-for shape in shapes:
-    print(f"{shape.area()}cm^2")
+# for shape in shapes:
+#     print(f"{shape.area()}cm^2")
 
 
