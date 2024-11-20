9012bc80f100825e1d62f35f9c83f0af9c7d9d18
AugustLoving <auglov536@ihmeducation.se>
2024-11-20 14:28:22 +0100

    Alla uppgifter klara

--- upgifterOOP.py
+++ upgifterOOP.py
@@ -68,46 +68,108 @@
 # account.withdraw(50)
 # print(account.get_balance())
 
-# upg 5, 6
-class Animal():
-    def __init__(self, name):
-        self.name = name
+# upg 5, 6, 7
+# class Animal():
+#     def __init__(self, name):
+#         self.name = name
     
-    def make_sound(self):
-        return "some sound"
+#     def make_sound(self):
+#         return "some sound"
     
-    def describe(self):
-        print(f"This is an animal named {self.name}")
+#     def describe(self):
+#         print(f"This is an animal named {self.name}")
 
-class Dog(Animal):
-    def make_sound(self):
-        return "Woof!"
+# class Dog(Animal):
+#     def make_sound(self):
+#         return "Woof!"
     
-    def describe(self):
-        print(f"This is dog named {self.name}!")
+#     def describe(self):
+#         print(f"This is dog named {self.name}!")
 
-class Cat(Animal):
-    def make_sound(self):
-        return "Meow!"
+# class Cat(Animal):
+#     def make_sound(self):
+#         return "Meow!"
 
-    def describe(self):
-        print(f"This is cat named {self.name}!")
-
-
-def animal_sound(animals):
-    for animal in animals:
-        print(animal.make_sound())
+#     def describe(self):
+#         print(f"This is cat named {self.name}!")
 
 
+# def animal_sound(animals):
+#     for animal in animals:
+#         print(animal.make_sound())
 
 
-dog = Dog("Scooby")
-cat = Cat("Garfield")
-djur_laten = [dog, cat]
-animal_sound(djur_laten)
-dog.make_sound()
-cat.make_sound()
-dog.describe()
-cat.describe()
 
+
+# dog = Dog("Scooby")
+# cat = Cat("Garfield")
+# djur_laten = [dog, cat]
+# animal_sound(djur_laten)
+# dog.make_sound()
+# cat.make_sound()
+# dog.describe()
+# cat.describe()
+
+#upg 8
+# class Rectangle():
+#     def __init__(self, width, height):
+#         self.width = width
+#         self.height = height
+    
+#     def area(self):
+#         area = self.height * self.width
+#         print(f"Arean är: {area}cm") 
+
+#     def perimeter(self):
+#         omkrets = 2 * (self.height + self.width)
+#         print(f"Omkretsen är: {omkrets}cm") 
+
+# rektangel = Rectangle(10, 5)
+
+# rektangel.area()
+# rektangel.perimeter()
+
+#upg 9
+# class Car():
+#     def __init__(self, brand, model, mileage):
+#         self.brand = brand
+#         self.model = model
+#         self.mileage = mileage
+    
+#     def drive(self, kilometers):
+#         self.mileage += kilometers
+         
+
+# car1 = Car("Volkswagen", "Passat", 1000)
+# car1.drive(50)
+# print(car1.mileage)
+
+#upg 10
+
+# class Address():
+#     def __init__(self, street, city):
+#         self.street = street
+#         self.city = city
+
+# class Person():
+#     def __init__(self, name, age, address):
+#         self.name = name
+#         self.age = age
+#         self.address = address
+
+# address = Address("Treklövergatan", "västerås")
+# address2 = Address("Dennagata", "Göteborg")
+# person1 = Person("August Löving", 24, address)
+# person2 = Person("Göta Kanalsson", 50, address2)
+
+
+# print(person1.name)
+# print(person1.age)
+# print(person1.address.street)
+# print(person1.address.city)
+
+# print(person2.name)
+# print(person2.age)
+# print(person2.address.street)
+# print(person2.address.city)
 
