c2651f518f3291cf34bae29d7037cc7c251e30d7
AugustLoving <auglov536@ihmeducation.se>
2024-11-21 11:59:22 +0100

    ?

--- importochfromupg2.py
+++ importochfromupg2.py
@@ -1,7 +1,16 @@
-import matematik_test
+#Upg1
+# import matematik_test
 
-result = matematik_test.addera(10, 5)
-print(result)
+# result = matematik_test.addera(10, 5)
+# print(result)
 
-result2 = matematik_test.multiplicera(3, 3)
-print(result2)
+# result2 = matematik_test.multiplicera(3, 3)
+# print(result2)
+
+#Upg2
+import statistik
+
+tal_lista = [10, 20, 30, 40, 50]
+
+medel = statistik.medelvärde(tal_lista)
+print(medel)
