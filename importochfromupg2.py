fa3b7b11625437367e73901b86a7cd580132d8ae
AugustLoving <auglov536@ihmeducation.se>
2024-11-22 09:16:15 +0100

    save

--- importochfromupg2.py
+++ importochfromupg2.py
@@ -8,23 +8,27 @@
 # print(result2)
 
 #Upg2
-# from statistik import medelvarde, median, standardavvikelse
+from statistik import medelvarde, median, standardavvikelse
 
-# tal_lista = [10, 20, 30, 40, 50]
+tal_lista = [10, 20, 30, 40, 50]
 
-# medel = medelvarde(tal_lista)
-# print(medel)
+medel = medelvarde(tal_lista)
+print(medel)
 
-# median = median(tal_lista)
-# print(median)
+median = median(tal_lista)
+print(median)
+
+st_avv = standardavvikelse(tal_lista)
+print(st_avv)
 
 #upg3
-from geometriska import area_r, area_c
+# from geometriska import area_r, area_c
 
-rektangel_area = area_r(10, 5)
-print(rektangel_area)
+# rektangel_area = area_r(10, 5)
+# print(rektangel_area)
 
-cirkel_area = area_c(10)
-print(cirkel_area)
+# cirkel_area = area_c(10)
+# print(cirkel_area)
 
 #upg 4
+
---
+++ testing_katalog/filhantering.py
@@ -0,0 +1,8 @@
+def skriv_till_fill(filnamn, text):
+    with open(filnamn, 'w') as fil:
+        fil.write(text)
+
+def las_upp_fil(filnamn):
+    with open(filnamn, 'r') as fil:
+        return fil.read()
+    
